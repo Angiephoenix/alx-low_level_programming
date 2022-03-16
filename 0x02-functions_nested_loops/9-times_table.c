fb5b94529f97272a01229432edd098d27e310039
Angiephoenix <angiephoenix93@gmail.com>
2022-03-16 11:57:41 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ if ((c / 10) == 0)
 {
 if (b == 0)
 {
-_putchar (' ');
+_putchar ('0');
 }
 if (b != 0)
 {
@@ -27,7 +27,7 @@ _putchar ((c % 10) + '0');
 }
 if (b < 9)
 {
-_putchar (',');
+_putchar(',');
 _putchar (' ');
 }
 }
@@ -37,7 +37,7 @@ _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
 if (b < 9)
 {
-_putchar (',');
+_putchar(',');
 _putchar (' ');
 }
 }
